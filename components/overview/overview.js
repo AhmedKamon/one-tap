bdaaa22298b3d06b4d939e2be4cbd38c501bd89d
AhmedKamon <ahmedkamon3@gmail.com>
2021-08-21 15:21:40 +0600

    lets go

--- components/overview/overview.js
+++ components/overview/overview.js
@@ -24,8 +24,10 @@ const Overview = () => {
       </div>
       <div className="col-md-6">
         <div className={`${styles.card} mt-5 p-3`}>
-          <div className="text-primary p-3">
-            <h1>kamon</h1>
+          <div className={`${styles.cardText}  p-5`}>
+            <h6 className="fw-bold mb-5">QR Codes</h6>
+            <h6 className="fw-bold">20</h6>
+            <h6 className="fw-bold">SCENS</h6>
           </div>
         </div>
       </div>
