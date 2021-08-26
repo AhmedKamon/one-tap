d622fe827c14f760fa0a26fb8d9720ab2dece6f0
AhmedKamon <ahmedkamon3@gmail.com>
2021-08-27 01:16:32 +0600

    sagor

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import { BsStarFill } from "react-icons/bs";
-import { data } from "../../utilites/data";
-import icon from "../../utilites/images/Mask Group.svg";
-import styles from "./sidebar.module.css";
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { BsStarFill } from 'react-icons/bs';
+import { data } from '../../utilites/data';
+import icon from '../../utilites/images/Mask Group.svg';
+import styles from './sidebar.module.css';
 
 const Sidebar = () => {
   return (
@@ -16,7 +16,7 @@ const Sidebar = () => {
 
         <ul className={`${styles.sideBarLogo} navbar-nav`}>
           {data.map((logo) => (
-            <li key={logo.id} className={`nav-item mt-2 `}>
+            <li key={logo.id} className={`nav-item mt-4 `}>
               <span className="d-flex align-items-center" href="">
                 <div className="me-3">
                   <Image src={logo.logo} height="20" width="20" alt="logo" />
@@ -30,7 +30,7 @@ const Sidebar = () => {
         </ul>
         <div className={`${styles.upgrade} text-center w-100`}>
           <button className={`${styles.btn} ms-5 mt-1`} type="">
-            <BsStarFill className="me-3" style={{ fontSize: "25px" }} /> UPGRADE
+            <BsStarFill className="me-3" style={{ fontSize: '25px' }} /> UPGRADE
             NOW
           </button>
         </div>
