9dd5ba6bfcdd3e0b6e62c368524ba3b62211c840
AhmedKamon <ahmedkamon3@gmail.com>
2021-09-01 11:31:10 +0600

    new

--- pages/overview.js
+++ pages/overview.js
@@ -14,39 +14,41 @@ const Overview = () => {
         <div className="col-md-10">
           <Navbar />
           <Scrollbar style={{ width: '76vw', height: '80vh' }}>
-            <div className="row pb-3 container">
-              <div className="col-md-6">
-                <div className={`${styles.card} mt-5 `}>
-                  <div className={`${styles.cardText}  p-5`}>
-                    <h6 className="fw-bold mb-5 fs-22 lh-26">QR Codes</h6>
-                    <h6 className="fw-bold fs-46 lh-66">20</h6>
-                    <h6 className="fw-bold fs-18 lh-26">SCENS</h6>
-                  </div>
+            <div className="row my-5 ms-5">
+              <div className={` col-md-5 mb-5`}>
+                <div
+                  className={`${styles.card} d-flex flex-column justify-content-center align-items-center`}
+                >
+                  <h6 className="fw-bold mb-5 fs-22 lh-26">QR Codes</h6>
+                  <h6 className="fw-bold fs-46 lh-66">20</h6>
+                  <h6 className="fw-bold fs-18 lh-26">SCENS</h6>
                 </div>
               </div>
-
-              <div className="col-md-6">
-                <div className={`${styles.card} mt-5 `}>
-                  <div className={`${styles.cardText}  p-5`}>
-                    <h6 className="fw-bold mb-5 fs-22 lh-26">projects</h6>
-                    <h6 className="fw-bold fs-46 lh-66">20</h6>
-                  </div>
+              <div className={` col-md-5 mb-5`}>
+                <div
+                  className={`${styles.card} d-flex flex-column justify-content-center align-items-center`}
+                >
+                  <h6 className="fw-bold mb-5 fs-22 lh-26">QR Codes</h6>
+                  <h6 className="fw-bold fs-46 lh-66">20</h6>
+                  <h6 className="fw-bold fs-18 lh-26">SCENS</h6>
                 </div>
               </div>
-              <div className="col-md-6">
-                <div className={`${styles.card} mt-5`}>
-                  <div className={`${styles.cardText}  p-5`}>
-                    <h6 className="fw-bold mb-5 fs-22 lh-26">Landing Page</h6>
-                    <h6 className="fw-bold fs-46 lh-66">12</h6>
-                  </div>
+              <div className={` col-md-5 mb-5`}>
+                <div
+                  className={`${styles.card} d-flex flex-column justify-content-center align-items-center`}
+                >
+                  <h6 className="fw-bold mb-5 fs-22 lh-26">QR Codes</h6>
+                  <h6 className="fw-bold fs-46 lh-66">20</h6>
+                  <h6 className="fw-bold fs-18 lh-26">SCENS</h6>
                 </div>
               </div>
-              <div className="col-md-6">
-                <div className={`${styles.card} mt-5 `}>
-                  <div className={`${styles.cardText}  p-5`}>
-                    <h6 className="fw-bold mb-5 fs-22 lh-26">Business Card</h6>
-                    <h6 className="fw-bold fs-46 lh-66">20</h6>
-                  </div>
+              <div className={` col-md-5 mb-5`}>
+                <div
+                  className={`${styles.card} d-flex flex-column justify-content-center align-items-center`}
+                >
+                  <h6 className="fw-bold mb-5 fs-22 lh-26">QR Codes</h6>
+                  <h6 className="fw-bold fs-46 lh-66">20</h6>
+                  <h6 className="fw-bold fs-18 lh-26">SCENS</h6>
                 </div>
               </div>
             </div>
--- pages/projects.js
+++ pages/projects.js
@@ -7,37 +7,38 @@ import NextLink from 'next/link';
 
 const Projects = () => {
   return (
-    <div className="container vh-100">
+    <div className="container">
       <div className="row">
         <div className="col-md-2">
           <Sidebar />
         </div>
         <div className="col-md-10 ">
           <Navbar />
-          <Scrollbar style={{ width: '60vw', height: '85vh' }}>
-            <div className="container mt-5 main pe-5">
-              <div className="d-flex justify-content-between">
-                <div className="col-md-6  position-relative ">
-                  <input
-                    className={`projectInput form-control`}
-                    type="text"
-                    placeholder="enter your project name"
-                    name="enter your project name"
-                  />
-                  <FaSearch className="searchIcon" />
-                </div>
-                <div className="col-md-3 text-center ms-5">
-                  <button className="project-btn fs-18 lh-12 py-3" type="">
-                    Filter
-                  </button>
-                </div>
-                <div className="col-md-3 text-center">
-                  <button className="project-btn" type="">
-                    Last 7 days
-                  </button>
-                </div>
-              </div>
 
+          <div className="mt-5 pe-5">
+            {/* <div className="d-flex justify-content-between">
+              <div className="col-md-6  position-relative ">
+                <input
+                  className={`projectInput form-control`}
+                  type="text"
+                  placeholder="enter your project name"
+                  name="enter your project name"
+                />
+                <FaSearch className="searchIcon" />
+              </div>
+              <div className="col-md-3 text-center ms-5">
+                <button className="project-btn fs-18 lh-12 py-3" type="">
+                  Filter
+                </button>
+              </div>
+              <div className="col-md-3 text-center">
+                <button className="project-btn" type="">
+                  Last 7 days
+                </button>
+              </div>
+            </div> */}
+            {/* ==================== */}
+            <div className="scroll">
               <div className="bg-project p-5">
                 <div className="ps-4">
                   <h6 className="fw-bold">ALL PROJECTS</h6>
@@ -74,7 +75,9 @@ const Projects = () => {
                 </div>
               </div>
             </div>
-          </Scrollbar>
+
+            {/* ==================== */}
+          </div>
         </div>
       </div>
     </div>
